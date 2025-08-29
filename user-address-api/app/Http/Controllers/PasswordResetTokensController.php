b1d7accb3c3370b6d636f63c5ed2da2e6ac6b39e
TiagoFollador <tiagobritofollador@gmail.com>
2025-08-28 21:47:23 -0300

    implementation of password reset tokens migration

---
+++ user-address-api/app/Http/Controllers/PasswordResetTokensController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Password_reset_tokens;
+use Illuminate\Http\Request;
+
+class PasswordResetTokensController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Password_reset_tokens $password_reset_tokens)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Password_reset_tokens $password_reset_tokens)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Password_reset_tokens $password_reset_tokens)
+    {
+        //
+    }
+}
