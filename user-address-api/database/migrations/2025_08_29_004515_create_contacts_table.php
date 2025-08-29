d20706d7432124cadcd15540793e4bb4e3e1b7f2
TiagoFollador <tiagobritofollador@gmail.com>
2025-08-28 21:46:38 -0300

    implementation of contacts migration

---
+++ user-address-api/database/migrations/2025_08_29_004515_create_contacts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->onDelete('cascade');
+
+            $table->string('name');
+            $table->string('cpf', 14)->unique();
+            $table->string('phone', 20);
+            $table->string('cep', 9);
+            $table->string('street');
+            $table->string('number', 20);
+            $table->string('complement')->nullable();
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state', 2);
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
